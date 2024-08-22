e091026690b378c523f3aafaeada2de0e2b4e583
robiulalamdev <robiulalam762400@gmail.com>
2024-08-22 19:49:24 +0600

    facebooklogin componenet udpate

--- components/auth/FacebookLogin.js
+++ components/auth/FacebookLogin.js
@@ -89,20 +89,6 @@ const FacebookLogin = () => {
           Login with Facebook
         </Text>
       </Pressable>
-      {/* <LoginButton
-        onLoginFinished={(error, result) => {
-          if (error) {
-            console.log("login has error: " + result.error);
-          } else if (result.isCancelled) {
-            console.log("login is cancelled.");
-          } else {
-            AccessToken.getCurrentAccessToken().then((data) => {
-              console.log(data.accessToken.toString());
-            });
-          }
-        }}
-        onLogoutFinished={() => console.log("logout.")}
-      /> */}
     </View>
   );
 };
